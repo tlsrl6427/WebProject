3284af5cb3bee2c315b7dd10954820a6edf38c11
tlsrl6427 <tlsr6427@naver.com>
2020-11-30 12:48:18 +0900

    google & firebase login

---
+++ public/googlelogin.js
@@ -0,0 +1,26 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDwMYnkiXywcpwhC8OX0iQGHRwdZLnEm2o",
+    authDomain: "webproject-f85df.firebaseapp.com",
+    databaseURL: "https://webproject-f85df.firebaseio.com",
+    projectId: "webproject-f85df",
+    storageBucket: "webproject-f85df.appspot.com",
+    messagingSenderId: "505722645824",
+    appId: "1:505722645824:web:8edc2cb13f06ccf175dea5",
+    measurementId: "G-HFWKZR7F96"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+/*
+firebase.auth().signOut(provider).then(function() {
+  // Sign-out successful.
+  alert("로그아웃되었습니다")
+}).catch(function(error) {
+  // An error happened.
+});
+*/
