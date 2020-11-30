aeb7c24dcf7cdcc25c09f431a65f4f485dfc5522
tlsrl6427 <tlsr6427@naver.com>
2020-11-30 12:59:43 +0900

    Login Popup

---
+++ public/openPopup.js
@@ -0,0 +1,4 @@
+function openWindowPop(url, name){
+    var options = 'top=200, left=500, width=250, height=120, status=no, menubar=no, toolbar=no, resizable=no, fullscreen=no, location=no';
+    window.open(url, name, options);
+}
