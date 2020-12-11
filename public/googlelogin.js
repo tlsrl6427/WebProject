ff7cb9ed64b76d08beb364f1c27c090faf429084
tlsrl6427 <tlsr6427@naver.com>
2020-12-11 11:36:33 +0900

    Auto button change

--- public/googlelogin.js
+++ public/googlelogin.js
@@ -1,3 +1,4 @@
+/*
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/storage";
@@ -5,7 +6,7 @@ import "firebase/database";
 import React, { useCallback } from "react";
 import { useFirebase } from "react-redux-firebase";
 import { useHistory } from "react-router-dom";
-
+*/
 var firebaseConfig = {
   apiKey: "AIzaSyDwMYnkiXywcpwhC8OX0iQGHRwdZLnEm2o",
   authDomain: "webproject-f85df.firebaseapp.com",
